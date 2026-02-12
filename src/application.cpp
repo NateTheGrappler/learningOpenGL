fea02a66c901500985c82fd78d62781942f4b673
NateTheGrappler <guzinskinatan@gmail.com>
2026-02-12 11:53:57 -0500

    set up opening a window and added more stuff to complete project

--- src/application.cpp
+++ src/application.cpp
@@ -2,18 +2,76 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+
+//settings
+const unsigned int screenWidth  = 800;
+const unsigned int screenHeight = 600;
+
 int main(void)
 {
-    glfwInit();                                       //init the window
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);    //configure whatever is specified, and then choosen option from given enum
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);    //configure whatever is specified, and then choosen option from given enum
+    glfwInit();                                                                         //init the window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                                      //configure whatever is specified, and then choosen option from given enum
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                                      //configure whatever is specified, and then choosen option from given enum
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", NULL, NULL);         //intialize the window, and then make sure that it was created successfully
     if (window == NULL)
     {
         std::cout << "Failed to create the window you goober" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+   
+    //set up changing the viewport whenever you want to resize
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))                            //Initialize the glad library
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);                                                         //set the viewport of the window so that way open gl knows the size and position of the render
+
+
+    //main while loop for rendering
+    while (!glfwWindowShouldClose(window))
+    {
+        //process input
+        processInput(window);
+
+        //render functions
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //swap render buffers and poll key press events
+        glfwSwapBuffers(window); 
+        glfwPollEvents();        
     }
 
+
+
+    //close glfw cleanly
+    glfwTerminate();
     return 0;
+}
+
+
+//function to handle viewport resizing with window resizing
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+//handle input detected from user
+void processInput(GLFWwindow* window)
+{
+    //if escape key pressed, close the window
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
